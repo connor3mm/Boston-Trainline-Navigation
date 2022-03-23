153f946a0f67dd01c206149718b641f4fb8db52a
Ramsey Ediku CS2019 <ramsey.ediku.2020@uni.strath.ac.uk>
2022-03-23 15:05:05 +0000

    added UI to the app

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        primaryStage.setTitle("MVC Example App");
+        primaryStage.setScene(new Scene(root, 500, 1000));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/MetroController.java
+++ src/MetroController.java
@@ -1,2 +1,11 @@
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class MetroController {
+
+    @FXML private MenuButton departureOption;
+    @FXML private MenuButton destinationOption;
+    @FXML private Button FindRouteButton;
+    @FXML private TextArea routeResult;
 }
